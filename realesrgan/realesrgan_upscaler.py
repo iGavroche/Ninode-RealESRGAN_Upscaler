35bca6ebebd5a9cda558eca9de62ce4e5be61b2e
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-08 20:40:24 +0100

    Add files via upload

--- realesrgan/realesrgan_upscaler.py
+++ realesrgan/realesrgan_upscaler.py
@@ -11,7 +11,7 @@ import gc
 import pathlib
 
 # Set some strings.
-__version__ = "0.0.0.7"
+__version__ = "0.0.0.8"
 
 # Import the third party Python modules.
 from PIL import Image
@@ -56,27 +56,34 @@ SCRIPT_PATH = pathlib.Path(__file__).parent.resolve()
 PARENT_PATH = SCRIPT_PATH.parent.absolute()
 MODELS_PATH = ''.join([str(PARENT_PATH), "/models"])
 
-# Set the model.
+# Set the models.
 #MODEL = "4x-UltraSharp.pth"
 MODEL = "RealESRGAN_x4plus.pth"
 
 # Set the model path.
-MODEL_PATH = '/'.join([str(MODELS_PATH), MODEL])
+#MODEL_PATH = '/'.join([str(MODELS_PATH), "RealESRGAN_x4plus.pth"])
 
 # Set file path.
-FILE_URL = 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth'
+MODELS = ['RealESRGAN_x4plus.pth', 'RealESRGAN_x2plus.pth', 'RealESRNet_x4plus.pth']
+FILE_URL = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth',
+            'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth',
+            'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.1/RealESRNet_x4plus.pth']
 
-# Download model.
-model_file = pathlib.Path(MODEL_PATH)
-print(model_file)
-if not model_file.is_file():
-    response = requests.get(FILE_URL, timeout=20)
-    if response.status_code == 200:
-        with open(MODEL_PATH, 'wb') as file:
-            file.write(response.content)
-        print('File download succeeded!')
-    else:
-        print('File download failed!')
+# Download models.
+count = 0
+for mod in MODELS:
+    model_path = '/'.join([str(MODELS_PATH), mod])
+    model_file = pathlib.Path(model_path)
+    print(model_file)
+    if not model_file.is_file():
+        response = requests.get(FILE_URL[count], timeout=20)
+        if response.status_code == 200:
+            with open(model_path, 'wb') as file:
+                file.write(response.content)
+            print('File download succeeded!')
+        else:
+            print('File download failed!')
+        count += 1
 
 # -------------------------------
 # Convert Tensor to PIL function.
@@ -97,20 +104,22 @@ def pil2tensor(image):
 # ----------------------------
 # RealESRGAN Upscaler function
 # ----------------------------
-def upscaler(input_img, outscale, gpu_id, tile, fp_fmt):
+def upscaler(input_img, outscale, gpu_id, tile, fp_fmt, denoise, netscale, tile_pad, pre_pad, models):
     """Inference upscaler using Real-ESRGAN.
     """
+    MODEL_PATH = '/'.join([str(MODELS_PATH), models])
     # Convert PIL image to Numpy array.
     image = np.array(input_img)
     # Set up the network for the RealESRGAN model.
-    model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+    model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=netscale)
     # Set the netscale (4) to the depending one of the model (x4).
-    netscale = 4
+    #netscale = 4
     # Set dni_weight to control the denoise strength.
-    dni_weight = None
+    #dni_weight = None
+    dni_weight = [denoise, 1 - denoise]
     # Set some padding values.
-    pre_pad = 0
-    tile_pad = 10
+    #pre_pad = 0
+    #tile_pad = 10
     # Define the upscaler upsampler.
     with ClearCache():
         upsampler = RealESRGANer(
@@ -150,10 +159,14 @@ class RealEsrganUpscaler:
             "required": {
                 "image": ("IMAGE",),
                 "scale_factor": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1024.0, "step": 0.1}),
-                "gpu_id": (GPU_LIST, {}),
                 "tile_number": ("INT", {"default": 0, "min": 0, "max": 8192, "step": 64}),
+                "tile_pad": ("INT", {"default": 10, "min": 0, "max": 1024, "step": 1}),
+                "pre_pad": ("INT", {"default": 0, "min": 0, "max": 1024, "step": 1}),
                 "fp_format": (["fp16", "fp32"], {}),
-                "model": ("STRING", {"multiline": False, "default": MODEL}),
+                "denoise": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0, "step": 0.01}),
+                "netscale": ("INT", {"default": 4, "min": 1, "max": 64, "step": 1}),
+                "gpu_id": (GPU_LIST, {}),
+                "models": (MODELS, {}),
             }
         }
 
@@ -165,7 +178,7 @@ class RealEsrganUpscaler:
     DESCRIPTION = "Upscaling using RealESRGAN."
     OUTPUT_NODE = True
 
-    def realesrgan_upscaler(self, image, gpu_id, scale_factor, tile_number, fp_format, model):
+    def realesrgan_upscaler(self, image, gpu_id, scale_factor, tile_number, fp_format, models, denoise, netscale, tile_pad, pre_pad):
         '''Detect ellipse.'''
         # Set value for paranoia reason.
         gpu_id = int(gpu_id)
@@ -174,7 +187,7 @@ class RealEsrganUpscaler:
         # Copy image.
         imgNEW = img_input.copy()
         # Upscale image.
-        imgNEW = upscaler(imgNEW, scale_factor, gpu_id, tile_number, fp_format)
+        imgNEW = upscaler(imgNEW, scale_factor, gpu_id, tile_number, fp_format, denoise, netscale, tile_pad, pre_pad, models)
         # Create tensors from PIL.
         image_out = pil2tensor(imgNEW)
         # Return the upscaled image.
