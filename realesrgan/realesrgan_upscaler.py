4b8fa35ea3eed8f97e9b4820a3ec793aa2a0b243
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-08 18:14:40 +0100

    Add files via upload

--- realesrgan/realesrgan_upscaler.py
+++ realesrgan/realesrgan_upscaler.py
@@ -10,6 +10,9 @@ import warnings
 import gc
 import pathlib
 
+# Set some strings.
+__version__ = "0.0.0.4"
+
 # Import the third party Python modules.
 from PIL import Image
 import numpy as np
@@ -20,9 +23,6 @@ from realesrgan import RealESRGANer
 
 # Disable future warning.
 warnings.filterwarnings("ignore", category=FutureWarning)
-
-# Set some strings.
-__version__ = "0.0.0.2"
 
 # Create context manager.
 class ClearCache:
@@ -57,6 +57,7 @@ PARENT_PATH = SCRIPT_PATH.parent.absolute()
 MODELS_PATH = ''.join([str(PARENT_PATH), "/models"])
 
 # Set the model.
+#MODEL = "4x-UltraSharp.pth"
 MODEL = "RealESRGAN_x4plus.pth"
 
 # Set the model path.
@@ -99,12 +100,11 @@ def pil2tensor(image):
 def upscaler(input_img, outscale, gpu_id, tile, fp_fmt):
     """Inference upscaler using Real-ESRGAN.
     """
-    print(type(input_img))
-    # Numpy array to opencv image.
+    # Convert PIL image to Numpy array.
     image = np.array(input_img)
     # Set up the network for the RealESRGAN model.
     model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-    # Set the netscale to the depending one of the model.
+    # Set the netscale (4) to the depending one of the model (x4).
     netscale = 4
     # Set dni_weight to control the denoise strength.
     dni_weight = None
@@ -129,11 +129,11 @@ def upscaler(input_img, outscale, gpu_id, tile, fp_fmt):
     except RuntimeError as error:
         torch.cuda.empty_cache()
         #raise Error("An serious error occurred 💥. " +
-        #            "CUDA out of memory. Try to set tile size to a smaller number.", duration=5, print_exception=False)
+        #            "CUDA out of memory. Try to set tile size to a smaller number.")
     except UnboundLocalError as error:
         torch.cuda.empty_cache()
         #raise Error("An serious error occurred 💥. " +
-        #            "Image size problem occurs. Tile size problem occurs." , duration=5, print_exception=False)
+        #            "Image size problem occurs. Tile size problem occurs.")
     # Return the upscaled image.
     return outimg
 
@@ -151,8 +151,9 @@ class RealEsrganUpscaler:
                 "image": ("IMAGE",),
                 "scale_factor": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1024.0, "step": 0.1}),
                 "gpu_id": (GPU_LIST, {}),
-                "tile_number": ("INT", {"default": 0, "min": 0, "max": 8192, "step": 256}),
+                "tile_number": ("INT", {"default": 0, "min": 0, "max": 1024, "step": 1}),
                 "fp_format": (["fp16", "fp32"], {}),
+                "model": ("STRING", {"multiline": False, "default": MODEL}),
             }
         }
 
@@ -164,7 +165,7 @@ class RealEsrganUpscaler:
     DESCRIPTION = "Upscaling using RealESRGAN."
     OUTPUT_NODE = True
 
-    def realesrgan_upscaler(self, image, gpu_id, scale_factor, tile_number, fp_format):
+    def realesrgan_upscaler(self, image, gpu_id, scale_factor, tile_number, fp_format, model):
         '''Detect ellipse.'''
         # Set value for paranoia reason.
         gpu_id = int(gpu_id)
