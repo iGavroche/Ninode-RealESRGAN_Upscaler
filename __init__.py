a6b6ae983d706c15681ca97db6ccb1ec16719d70
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-09 15:23:22 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 
 # Import the Python modules.
 from .realesrgan.realesrgan_upscaler import *
-from .realesrgan.showdatanodes import *
+from .realesrgan.show_data import *
 
 NODE_CLASS_MAPPINGS = { 
     "🚀 Universal RealESRGAN Upscaler": RealEsrganUpscaler,
@@ -13,4 +13,5 @@ WEB_DIRECTORY = "./js"
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
 
+# Print message into the terminal window.
 print("\033[34mComfyUI RealEsrganUpscaler Nodes: \033[92mLoaded\033[0m")
